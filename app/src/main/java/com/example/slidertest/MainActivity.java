90c530ed78ec19d42388a6cda7d266b520ba4479
s183725 <51827251+s183725@users.noreply.github.com>
2021-03-17 19:41:05 +0100

    Methods and Sliders

--- app/src/main/java/com/example/slidertest/MainActivity.java
+++ app/src/main/java/com/example/slidertest/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.slidertest;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.net.sip.SipSession;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.DragEvent;
 import android.view.View;
-import android.widget.NumberPicker;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
 
+//MULTITOGGLEBUTTON LIBS
 import com.google.android.material.slider.Slider;
 import com.google.android.material.snackbar.Snackbar;
 import com.sha.kamel.multitogglebutton.MultiToggleButton;
@@ -17,68 +22,100 @@ import com.sha.kamel.multitogglebutton.Selected;
 import com.sha.kamel.multitogglebutton.ToggleButton;
 
 
-public class MainActivity extends Activity {
 
-    public int warmVal;
-    public int coldVal;
-
-    private Slider warmSlider;
-    private Slider coldSlider;
+public class MainActivity extends AppCompatActivity {
+    
+    final int warm = 0;
+    final int cold = 1;
+    
+    private int[] sliderValue = new int[2];
+    private Slider[] sliders = new Slider[2];
+    
     private MultiToggleButton mtb1;
+    private TextView slideText;
 
+    private Calendar nowTime;
+    private SimpleDateFormat dateFormat;
+    private String date;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mtb1 = (MultiToggleButton) findViewById(R.id.mtb);
         mtb1.selectFirstItem(true);
+        nowTime = Calendar.getInstance();
 
-        warmSlider = (Slider) findViewById(R.id.warmSlider);
-        coldSlider = (Slider) findViewById(R.id.coldSlider);
+        sliders[warm] = (Slider) findViewById(R.id.warmSlider);
+        sliders[cold] = (Slider) findViewById(R.id.coldSlider);
+        sliders[warm].addOnChangeListener(warmListen);
+        sliders[cold].addOnChangeListener(coldListen);
 
-        warmVal = (int) warmSlider.getValue();
-        coldVal = (int) coldSlider.getValue();
-
-        mtb1.setOnItemSelectedListener(mtbSelect);
-
+        slideText = (TextView) findViewById(R.id.slideText) ;
+        sliderValue[warm] = (int) sliders[warm].getValue();
+        sliderValue[cold] = (int) sliders[cold].getValue();
+        
+        mtb1.setOnItemSelectedListener(mtbListen);
     }
 
     // WARM SLIDER CONFIGURATION
     private final Slider.OnChangeListener warmListen = new Slider.OnChangeListener() {
         @Override
-        public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
-            warmVal = (int) warmSlider.getValue();
+        public void onValueChange(@NonNull Slider warmSlider, float value, boolean fromUser) {
+            sliderValue[warm] = (int) warmSlider.getValue();
+            String textString = "Warm: " + sliderValue[warm] + ", Cold: " + sliderValue[cold];
+            slideText.setText(textString);
         }
     };
 
     // COLD SLIDER CONFIGURATION
     private final Slider.OnChangeListener coldListen = new Slider.OnChangeListener() {
         @Override
-        public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
-            coldVal = (int) coldSlider.getValue();
+        public void onValueChange(@NonNull Slider coldSlider, float value, boolean fromUser) {
+            sliderValue[cold] = (int) coldSlider.getValue();
+            String textString = "Warm: " + sliderValue[warm] + ", Cold: " + sliderValue[cold];
+            slideText.setText(textString);
         }
     };
 
-    // TOGGLEBUTTON CONFIGURATION
-    private final MultiToggleButton.OnItemSelectedListener mtbSelect = new ToggleButton.OnItemSelectedListener() {
+    // TOGGLEBUTTON LISTENER AND CONDITIONALS
+    private final MultiToggleButton.OnItemSelectedListener mtbListen = new ToggleButton.OnItemSelectedListener() {
         @Override
         public void onSelected(ToggleButton toggleButton, View item, int position, String label, boolean selected) {
-
-            if ( position == 2) {
-
-            } else if ( position == 3) {
-
+            if ( selectedItem(mtb1) == 2) {
+                slidersOn(sliders, false);
+            } else if ( selectedItem(mtb1) == 3) {
+                slidersOn(sliders, true);
             } else {
-                
+                slidersOn(sliders, false);
+                slideText.setAlpha(0.5f);
             }
-
         }
     };
 
-    private static void showSnackbar(View view, @StringRes int messageRes) {
-        Snackbar.make(view, messageRes, Snackbar.LENGTH_SHORT).show();
+    // GET MULTITOGGLEBUTTON SELECTED ITEM
+    private int selectedItem(ToggleButton toggleButton) {
+        Selected select = toggleButton.getSelected();
+        return select.getSingleItemPosition();
     }
 
+    // SLIDER ON/OFF SWITCH FOR state
+    private void slidersOn(Slider[] sliders, boolean state) {
+        if (state = true) {
+            sliders[warm].setEnabled(true);
+            sliders[warm].setAlpha(1.0f);
+            sliders[cold].setEnabled(true);
+            sliders[cold].setAlpha(1.0f);
+            findViewById(R.id.imageView2).setAlpha(1.0f);
+        } else {
+            sliders[warm].setEnabled(false);
+            sliders[warm].setAlpha(0.5f);
+            sliders[cold].setEnabled(false);
+            sliders[cold].setAlpha(0.5f);
+            findViewById(R.id.imageView2).setAlpha(0.5f);
+        }
+    }
 
 }
 
