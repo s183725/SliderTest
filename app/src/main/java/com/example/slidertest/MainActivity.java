2a3c41951c18fa7077c2361db6a4525b38b15acc
s183725 <51827251+s183725@users.noreply.github.com>
2021-03-20 23:00:00 +0100

    Functional sliders and "get time"

--- app/src/main/java/com/example/slidertest/MainActivity.java
+++ app/src/main/java/com/example/slidertest/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.util.Log;
+import android.widget.Toolbar;
 
 //MULTITOGGLEBUTTON LIBS
 import com.google.android.material.slider.Slider;
@@ -21,8 +22,10 @@ import com.sha.kamel.multitogglebutton.MultiToggleButton;
 import com.sha.kamel.multitogglebutton.Selected;
 import com.sha.kamel.multitogglebutton.ToggleButton;
 
+import java.util.stream.Stream;
 
 
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class MainActivity extends AppCompatActivity {
     
     final int warm = 0;
@@ -33,8 +36,9 @@ public class MainActivity extends AppCompatActivity {
     
     private MultiToggleButton mtb1;
     private TextView slideText;
+    private Selected select;
 
-    private Calendar nowTime;
+    private Calendar nowTime = Calendar.getInstance();
     private SimpleDateFormat dateFormat;
     private String date;
 
@@ -43,20 +47,19 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mtb1 = (MultiToggleButton) findViewById(R.id.mtb);
-        mtb1.selectFirstItem(true);
-        nowTime = Calendar.getInstance();
 
         sliders[warm] = (Slider) findViewById(R.id.warmSlider);
         sliders[cold] = (Slider) findViewById(R.id.coldSlider);
-        sliders[warm].addOnChangeListener(warmListen);
-        sliders[cold].addOnChangeListener(coldListen);
-
         slideText = (TextView) findViewById(R.id.slideText) ;
+        initTextview();
+
+        mtb1 = (MultiToggleButton) findViewById(R.id.mtb);
+        mtb1.selectFirstItem(true);
+        mtb1.setOnItemSelectedListener(mtbListen);
+
         sliderValue[warm] = (int) sliders[warm].getValue();
         sliderValue[cold] = (int) sliders[cold].getValue();
-        
-        mtb1.setOnItemSelectedListener(mtbListen);
+
     }
 
     // WARM SLIDER CONFIGURATION
@@ -82,27 +85,29 @@ public class MainActivity extends AppCompatActivity {
     // TOGGLEBUTTON LISTENER AND CONDITIONALS
     private final MultiToggleButton.OnItemSelectedListener mtbListen = new ToggleButton.OnItemSelectedListener() {
         @Override
-        public void onSelected(ToggleButton toggleButton, View item, int position, String label, boolean selected) {
-            if ( selectedItem(mtb1) == 2) {
+        public void onSelected(ToggleButton toggleButton, View item, int position, String label, boolean Selected) {
+            if ( toggleButton.getSelected().getSingleItemPosition() == 1 ) {
                 slidersOn(sliders, false);
-            } else if ( selectedItem(mtb1) == 3) {
+                slideText.setText("" + nowTime.getTime());
+            } else if ( toggleButton.getSelected().getSingleItemPosition() == 2 ) {
+                initTextview();
                 slidersOn(sliders, true);
+                sliders[warm].addOnChangeListener(warmListen);
+                sliders[cold].addOnChangeListener(coldListen);
             } else {
                 slidersOn(sliders, false);
-                slideText.setAlpha(0.5f);
             }
         }
     };
 
     // GET MULTITOGGLEBUTTON SELECTED ITEM
     private int selectedItem(ToggleButton toggleButton) {
-        Selected select = toggleButton.getSelected();
         return select.getSingleItemPosition();
     }
 
     // SLIDER ON/OFF SWITCH FOR state
     private void slidersOn(Slider[] sliders, boolean state) {
-        if (state = true) {
+        if (state == true) {
             sliders[warm].setEnabled(true);
             sliders[warm].setAlpha(1.0f);
             sliders[cold].setEnabled(true);
@@ -110,12 +115,18 @@ public class MainActivity extends AppCompatActivity {
             findViewById(R.id.imageView2).setAlpha(1.0f);
         } else {
             sliders[warm].setEnabled(false);
-            sliders[warm].setAlpha(0.5f);
+            sliders[warm].setAlpha(0.3f);
             sliders[cold].setEnabled(false);
-            sliders[cold].setAlpha(0.5f);
-            findViewById(R.id.imageView2).setAlpha(0.5f);
+            sliders[cold].setAlpha(0.3f);
+            findViewById(R.id.imageView2).setAlpha(0.3f);
         }
     }
 
+    private void initTextview() {
+        String textString = "Warm: " + sliderValue[warm] + ", Cold: " + sliderValue[cold];
+        slideText.setText(textString);
+    }
+
 }
+
 
